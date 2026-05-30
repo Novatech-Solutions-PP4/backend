f88ebf5ac17f0dc60cebae0290213856ebc7e674
sanvalos <santiago.avalos@outlook.com>
2026-05-30 00:35:21 +0000

    feat: add stored procedure for unidades limpieza and modalidades servicio

---
+++ app/sql/seed_data.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE PROCEDURE seed_initial_laundry_data()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    -- Regular inserts without the conflict check
+    INSERT INTO unidades_limpieza (nombre) VALUES ('canasto'), ('acolchado'), ('calzado');
+    INSERT INTO modalidades_servicio (nombre) VALUES ('economico'), ('standar'), ('delicado');
+END;
+$$;
